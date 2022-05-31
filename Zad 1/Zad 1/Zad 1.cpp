e9b5d90398ef9789047e57e0eb1c2d49bcad8ff9
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2022-06-01 00:28:58 +0200

    finish zad1

--- Zad 1/Zad 1/Zad 1.cpp
+++ Zad 1/Zad 1/Zad 1.cpp
@@ -1,20 +1,28 @@
-// Zad 1.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <string>
+
+bool is_palindrome(const std::string& s);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::string temp;
+
+	std::cout << "input word <q, to quit>:\n";
+	while (std::cin >> temp && temp != "q")
+	{
+		if (is_palindrome(temp))
+			std::cout << '\t' << temp << " - palindrome\n";
+		else
+			std::cout << '\t' << temp << " - not a palindrome\n";
+	
+		std::cout << "input word <q, to quit>:\n";
+	}
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool is_palindrome(const std::string& s)
+{
+	std::string temp = s;
+	std::reverse(temp.begin(), temp.end());
+	
+	return temp == s ? true : false;
+}
