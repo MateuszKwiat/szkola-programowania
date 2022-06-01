22bda6312ffc18b932bfd9d46496e321889ad28a
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2022-06-02 01:14:04 +0200

    new loop for Zad 8

--- Zad 8/Zad 8/Zad 8.cpp
+++ Zad 8/Zad 8/Zad 8.cpp
@@ -10,18 +10,28 @@ int main()
 	std::set<std::string> merge_list;
 	std::string temp;
 
-	std::cout << "Bolek's friends (q, to end the list):\n";
-	while (std::cin >> temp && temp != "q")
-		bolek_list.insert(temp);
+	std::cout << "would you like to fill Bolek's and Lolek's lists <y/n>:\n";
+	while (std::cin >> temp && temp != "n")
+	{
+		std::cout << "Bolek's friends (n, to end the list):\n";
+		while (std::cin >> temp && temp != "n")
+			bolek_list.insert(temp);
 
-	std::cout << "Lolek's friends (q, to end the list):\n";
-	while (std::cin >> temp && temp != "q")
-		lolek_list.insert(temp);
+		std::cout << "Lolek's friends (n, to end the list):\n";
+		while (std::cin >> temp && temp != "n")
+			lolek_list.insert(temp);
 
-	std::merge(bolek_list.begin(), bolek_list.end(), lolek_list.begin(), lolek_list.end(),
-		std::insert_iterator<std::set<std::string>>(merge_list, merge_list.begin()));
-	
-	std::cout << "Merged list:\n";
-	for (auto x : merge_list)
-		std::cout << '\t' << x << std::endl;
+		std::merge(bolek_list.begin(), bolek_list.end(), lolek_list.begin(), lolek_list.end(),
+			std::insert_iterator<std::set<std::string>>(merge_list, merge_list.begin()));
+		
+		std::cout << "Merged list:\n";
+		for (auto x : merge_list)
+			std::cout << '\t' << x << std::endl;
+
+		bolek_list.clear();
+		lolek_list.clear();
+		merge_list.clear();
+
+		std::cout << "Would you like to try again <y/n>:\n";
+	}
 }
