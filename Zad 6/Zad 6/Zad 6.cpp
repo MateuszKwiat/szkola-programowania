51785b22bb3af4486b26b7b5a2f54dd8df8e55cf
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2022-06-01 19:01:04 +0200

    Zad 6

--- Zad 6/Zad 6/Zad 6.cpp
+++ Zad 6/Zad 6/Zad 6.cpp
@@ -1,20 +1,85 @@
-// Zad 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <queue>
+#include "customer.h"
+
+const int MIN_PER_HR = 60;
+
+bool new_customer(double x);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::srand(std::time(0));
+
+	std::cout << "How long the queue can get: ";
+	int qs;
+	std::cin >> qs;
+	std::queue<Customer> line;
+
+	std::cout << "Simulation time (hours): ";
+	int hours;
+	std::cin >> hours;
+	long cycletime = MIN_PER_HR * hours;
+
+	std::cout << "Average amount of clients per hour: ";
+	double perhour;
+	std::cin >> perhour;
+	double min_per_cust;
+	min_per_cust = MIN_PER_HR / perhour;
+
+	Customer temp;
+	long turnawways = 0;
+	long customers = 0;
+	long served = 0;
+	long sum_line = 0;
+	int wait_time = 0;
+	long line_wait = 0;
+
+	for (int cycle = 0; cycle < cycletime; cycle++)
+	{
+		if (new_customer(min_per_cust))
+		{
+			if (line.size() >= qs)
+				turnawways++;
+			else
+			{
+				customers++;
+				temp.set(cycle);
+				line.push(temp);
+			}
+		}
+		if (wait_time <= 0 && !line.empty())
+		{
+			temp = line.front();
+			line.pop();
+			wait_time = temp.ptime();
+			line_wait += cycle - temp.when();
+			served++;
+		}
+		if (wait_time > 0)
+			wait_time--;
+		sum_line += line.size();
+	}
+	
+	if (customers > 0)
+	{
+		std::cout << "       Amount of customers: " << customers << std::endl;
+		std::cout << "Amount of served customers: " << served << std::endl;
+		std::cout << "       Amount of turnaways: " << turnawways << std::endl;
+		std::cout << "      Average queue length: ";
+		std::cout.precision(2);
+		std::cout.setf(std::ios_base::fixed, std::ios_base::floatfield);
+		std::cout.setf(std::ios_base::showpoint);
+		std::cout << (double)sum_line / cycletime << std::endl;
+		std::cout << "      Average waiting time: "
+			<< (double)line_wait / served << "minutes\n";
+	}
+	else
+		std::cout << "No clients\n";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+bool new_customer(double x)
+{
+	return (std::rand() * x / RAND_MAX < 1);
+}
