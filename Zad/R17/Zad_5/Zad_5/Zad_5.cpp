b3131087f2fc4f6df4ea30eda26c0c04fff3d297
Mateusz Kwiatkowski <mateusz.kwiat316@gmail.com>
2022-06-18 00:32:23 +0200

    zad 5

--- Zad/R17/Zad_5/Zad_5/Zad_5.cpp
+++ Zad/R17/Zad_5/Zad_5/Zad_5.cpp
@@ -1,20 +1,52 @@
-// Zad_5.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <set>
+#include <algorithm>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::ifstream finb("bolek.dat");
+	std::ifstream finl("lolek.dat");
+	std::ofstream fout("bolilol.dat");
+	std::set<std::string> bolek;
+	std::set<std::string> lolek;
+	std::set<std::string> bolek_and_lolek;
+	std::string temp1;
+	std::string temp2;
+
+	if (finb.is_open() && finl.is_open() && fout.is_open())
+	{
+		while (!(finb.eof() && finl.eof()))
+		{
+			finb >> temp1;
+			finl >> temp2;
+			bolek.insert(temp1);
+			lolek.insert(temp2);
+		}
+
+		std::cout << "Bolek's list:\n";
+		for (auto x : bolek)
+			std::cout << x << std::endl;
+
+		std::cout << "\nLolek's list:\n";
+		for (auto x : lolek)
+			std::cout << x << std::endl;
+
+		std::merge(bolek.begin(), bolek.end(), lolek.begin(), lolek.end(),
+			std::insert_iterator<std::set<std::string>>(bolek_and_lolek, bolek_and_lolek.begin()));
+		
+		for (auto x : bolek_and_lolek)
+			fout << x << std::endl;
+
+		finb.close();
+		finl.close();
+		fout.close();
+	}
+	else
+	{
+		std::cout << "Missing file";
+		exit(EXIT_FAILURE);
+	}
+
+
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
